9fab57e585f0d592d6fcbd457933f29d6802d3d7
Jialiang Ji <jlj070694@gmail.com>
2026-06-04 18:07:24 -0700

    typo

--- e1s/e1s_speed_check.py
+++ e1s/e1s_speed_check.py
@@ -14,7 +14,7 @@ PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 if PROJECT_ROOT not in sys.path:
     sys.path.insert(0, PROJECT_ROOT)
 
-from config.platfrom_config import load_config
+from config.platform_config import load_config
 
 cfg = load_config()
 
